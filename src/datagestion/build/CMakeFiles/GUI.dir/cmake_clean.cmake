83ddd381b50eaeb04f14c0f092d877222f63e351
Todin <112660117+Todin13@users.noreply.github.com>
2024-04-10 00:49:03 +0200

    localhost server foar API
    adding librarys and it's stucture plus a working test for creating a web server

---
+++ src/datagestion/build/CMakeFiles/GUI.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GUI.dir/main.cpp.o"
+  "CMakeFiles/GUI.dir/main.cpp.o.d"
+  "GUI"
+  "GUI.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GUI.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
