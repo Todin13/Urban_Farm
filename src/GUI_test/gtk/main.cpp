5e3feb234066148ee1f112744cb413b46474e962
Todin <112660117+Todin13@users.noreply.github.com>
2024-04-12 09:49:57 +0200

    update

--- src/GUI_test/gtk/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <gtkmm.h>
-#include "urbanfarm.h"
-
-int main(int argc, char* argv[])
-{
-  auto app = Gtk::Application::create("org.gtkmm.examples.base");
-
-  return app->make_window_and_run<MyWindow>(argc, argv);
-}
--- src/GUI_test/gtk/urbanfarm.cpp
+++
@@ -1,9 +0,0 @@
-#include "urbanfarm.h"
-#include <iostream>
-
-MyWindow::MyWindow()
-{
-  set_title("Urbanfarm_GUI");
-  set_default_size(1000, 1000);
-  
-}
